713c7f62f40564dc59e174081ac72cfc5aca4440
asanta27 <anthonysanta95@gmail.com>
2018-01-24 18:11:25 -0800

    Fix evolution method
    Fixed evolution method by implmenting a cache-grid to store the data of the next frame.

--- index.py
+++ index.py
@@ -2,17 +2,14 @@
 The universe of the Game of Life is an infinite two-dimensional orthogonal grid of square cells, each of which is in one of two possible states,
 alive or dead, or "populated" or "unpopulated". Every cell interacts with its eight neighbours, which are the cells that are horizontally,
 vertically, or diagonally adjacent. At each step in time, the following transitions occur:
-
 Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
 Any live cell with two or three live neighbours lives on to the next generation.
 Any live cell with more than three live neighbours dies, as if by overpopulation.
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
 Outline:
 1. Create Board
 2. Generate random cells
 3. Iterate through rules
-
 """
 
 import pygame
@@ -64,22 +61,26 @@ def infect_board(grid):
 def color_cell(color_state, row, column):
     pygame.draw.rect(screen,
                      color_state,
-                     [(cellx) * column,
-                      (celly) * row,
+                     [cellx * column,
+                      celly * row,
                       cellx,
                       celly])
 
 
-# Counts alive neighbors of chosen cell
+# Counts alive-neighbors of chosen cell
 def count_neighbors(grid, x, y):
     alive_neighbors = 0
+
     for i in neighbor_coordinates:
         # NeighborX and NeighborY coordinates
         nx = i[0]
         ny = i[1]
         try:
             if grid[x + nx][y + ny] == 1:
-                alive_neighbors += 1
+               # print("-----------")
+                # print("Coordinate: " + str(x), str(y) + " Neighbor: " + str(x + nx),str(y + ny))
+                alive_neighbors = alive_neighbors + 1
+                # print(alive_neighbors)
         except: # need to figure out how to avoid this error in the first place
             pass
     return alive_neighbors
@@ -87,26 +88,33 @@ def count_neighbors(grid, x, y):
 
 # Begins the evolution process
 def evolve(grid):
+    # grid_cache stores the new grid's data
+    grid_cache = [[0 for x in range(100)] for y in range(100)]
     for x in range(100):
         for y in range(100):
             alive_neighbors = count_neighbors(grid, x, y)
             if grid[x][y] == 1:
-                # alive cells with 2 or 3 neighbors live on, the rest die...
+                # alive cells with less than 2 neighbors but more than 3, DIE...
                 if alive_neighbors < 2 or alive_neighbors > 3:
-                    grid[x][y] = 0
+                    grid_cache[x][y] = 0
                     color_cell(dead, x, y)
-
-            elif grid[x][y] == 0:
-                # empty-cells with 3 neighbors become alive.
-                if alive_neighbors == 3:
-                    grid[x][y] = 1
+                else:
+                    # alive cells with 2 or 3 neighbors LIVE ON
+                    grid_cache[x][y] = grid[x][y]
                     color_cell(alive, x, y)
-
+            elif grid[x][y] == 0:
+                # empty-cells with 3 neighbors SPAWN
+                if alive_neighbors == 3:
+                    grid_cache[x][y] = 1
+                    color_cell(alive, x, y)
             else:
                 pass
+    # returns the latest grid
+    return grid_cache
 
 
 def main():
+    print("- Game Started -")
     # initialize game
     pygame.init()
     pygame.display.set_caption("Game Of Life")
@@ -131,6 +139,7 @@ def main():
 
     # ---- Main event loop ----
     while not done:
+
         pressed = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -155,7 +164,7 @@ def main():
             infected = True
 
         if run_program:
-            evolve(grid)
+            grid = evolve(grid)
             # Update screen
             pygame.display.flip()
         else:
